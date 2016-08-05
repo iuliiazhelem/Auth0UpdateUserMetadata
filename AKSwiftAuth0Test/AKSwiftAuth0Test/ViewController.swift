7e413a9687c1adefa9bc8b0f7baa9000ae41c125
Iuliia Zhelem <iuliia.zhelem@akvelon.com>
2016-08-06 02:30:27 +0300

    Swift: remove API v2

--- AKSwiftAuth0Test/AKSwiftAuth0Test/ViewController.swift
+++ AKSwiftAuth0Test/AKSwiftAuth0Test/ViewController.swift
@@ -10,10 +10,6 @@ import UIKit
 import Lock
 import Auth0
 
-//Please use your Auth0 APIv2 token from https://auth0.com/docs/api/management/v2/tokens
-//scopes : update:users
-let kAuth0APIv2Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJJdUFiSnZvZXpwZTFFWUM2ZVhRRUoyd0QwSm5MOE5IZSIsInNjb3BlcyI6eyJ1c2VycyI6eyJhY3Rpb25zIjpbInVwZGF0ZSJdfX0sImlhdCI6MTQ2ODA0NjQ1MSwianRpIjoiOTA5MmJiMzBiNTJhNWYxNDQ5NjQ0NjNiZjY3ODM3OWUifQ.YS24m0ywZo2B6Y2Wfu11zzudLHU-25XK3DIiSZPVldA"
-
 class ViewController: UIViewController {
     
     var userId:String?
@@ -45,76 +41,26 @@ class ViewController: UIViewController {
     }
     
     @IBAction func clickUpdateUserdataButton(sender: AnyObject) {
-        self.updateUserMetadataWithAPIRequest()
-    }
-
-    func updateUserMetadataWithAPIRequest() {
-        if let actualUserId = self.userId {
-            // PATCH request
-            // We need url "https://<Auth0 Domain>/api/v2/users/<user_id>"
-            // and header "Authorization : Bearer <APIv2 token>"
-            
-            let userDomain = (NSBundle.mainBundle().infoDictionary!["Auth0Domain"]) as! String
-            let userId = actualUserId.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
-            let urlString = "https://\(userDomain)/api/v2/users/\(userId)"
-            let url = NSURL(string: urlString)
-            if let actualUrl = url {
-                let request = NSMutableURLRequest(URL: actualUrl)
-                request.HTTPMethod = "PATCH";
-                request.allHTTPHeaderFields = ["Authorization" : "Bearer \(kAuth0APIv2Token)", "Content-Type" : "application/json"]
-                
-                //{"user_metadata" : {"name" : "<name>", "country" : "<country>"}}
-                let body = "{\"user_metadata\":{\"name\":\"\(self.nameMetadataTextField.text!)\",\"country\":\"\(self.countryMetadataTextField.text!)\"}}"
-                let bodyData:NSData = (body as NSString).dataUsingEncoding(NSUTF8StringEncoding)!
-                
-                request.HTTPBody = bodyData
-                
-                NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: {(data : NSData?, response : NSURLResponse?, error : NSError?) in
-                    
-                    // Check if data was received successfully
-                    if error == nil && data != nil {
-                        do {
-                            // Convert NSData to Dictionary where keys are of type String, and values are of any type
-                            let json = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! [String:AnyObject]
-                            print("\(json)")
-                            if let metadata = json["user_metadata"] {
-                                dispatch_async(dispatch_get_main_queue()) {
-                                    self.metadataText.text = "\(metadata)"
-                                    print("metadata: \(metadata)")
-                                }
-                            }
-                        } catch {
-                            let dataString = String(data: data!, encoding: NSUTF8StringEncoding)
-                            print("Oops something went wrong: \(dataString)")
-                        }
-                    } else {
-                        print("Oops something went wrong: \(error)")
-                    }
-                }).resume()
-            } else {
-                self.showMessage("Incorrect url");
-            }
-        } else {
-            self.showMessage("Please login first");
-        }
-    }
-    
-    func updateUserMetadataWithAuth0Toolkit() {
-        //DO NOT USE IT NOW
-        //TODO: correct creation of UserPatchAttributes!
-        let attributes:UserPatchAttributes? = nil
+        //TODO: correct creation of UserPatchAttributes
         //let attributes = UserPatchAttributes().userMetadata(metadata: ["name": "\(self.nameMetadataTextField.text!)", "country": "\(self.countryMetadataTextField.text!)"])
+        let attributes = ["name": "\(self.nameMetadataTextField.text!)", "country": "\(self.countryMetadataTextField.text!)"]
         if let actualToken = self.tokenId, let actualUserId = self.userId {
-        Auth0
-        .users(token: actualToken)
-        .patch(actualUserId, attributes: attributes!)
-        .start { result in
-                switch result {
-                case .Success(let profile):
-                    print("profile email: \(profile)")
-                case .Failure(let error):
-                    print(error)
-                }
+            Auth0
+                .users(token: actualToken)
+                //.patch(actualUserId, attributes: attributes)
+                .patch(actualUserId, userMetadata: attributes)
+                .start { result in
+                    switch result {
+                    case .Success(let profile):
+                        if let metadata = profile["user_metadata"] {
+                            dispatch_async(dispatch_get_main_queue()) {
+                                self.metadataText.text = "\(metadata)"
+                                print("metadata: \(metadata)")
+                            }
+                        }
+                    case .Failure(let error):
+                        print(error)
+                    }
             }
         }
     }
